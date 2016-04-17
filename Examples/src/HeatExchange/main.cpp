986d320eabeec931d87d82a27be8ba86f972cfe8
PACS Student <pacs_student@pacs-virtual-machine.(none)>
2016-04-17 16:58:46 +0200

    Challenge 1.3 : si usa l'algoritmo di Thomas per risolvere il sistema lineare tridiagonale

--- Examples/src/HeatExchange/main.cpp
+++ Examples/src/HeatExchange/main.cpp
@@ -4,7 +4,7 @@
 #include <tuple>
 #include "readParameters.hpp"
 #include "GetPot.hpp"
-//#include "gnuplot-iostream.hpp"// interface with gnuplot
+#include "gnuplot-iostream.hpp"// interface with gnuplot
 /*!
   @file main.cpp
   @brief Temperature distribution in a 1D bar.
@@ -15,7 +15,7 @@
     
     **************************************************
     Linear finite elements
-    Iterative resolution by Gauss Siedel.
+    Resolution by Thomas algorithm.
     **************************************************
     
     Example adapted by Luca Formaggia from  a code found in 
@@ -51,8 +51,6 @@ int main(int argc, char** argv)
   // Transfer parameters to local variables
   // I use references to save memory (not really an issue here, it is just
   // to show a possible  use of references)
-  const int&    itermax= param.itermax;   //max number of iteration for Gauss-Siedel
-  const double& toler=param.toler;   // Tolerance for stopping criterion
   // Here I use auto (remember that you need const and & if you want constant references)
   const auto& L= param.L;  // Bar length
   const auto& a1=param.a1; // First longitudinal dimension
@@ -61,9 +59,9 @@ int main(int argc, char** argv)
   const auto& Te=param.Te; // External temperature (Centigrades)
   const auto& k=param.k;  // Thermal conductivity
   const auto& hc=param.hc; // Convection coefficient
-  const auto&    M=param.M; // Number of grid elements
+  const auto& M=param.M; // Number of grid elements
   const auto& result_name=param.result_name; // name of the output file
-  const auto& norm=param.norm; //norm for the stopping criterion
+  
   //! Precomputed coefficient for adimensional form of equation
   const auto act=2.*(a1+a2)*hc*L*L/(k*a1*a2);
 
@@ -71,98 +69,46 @@ int main(int argc, char** argv)
   const auto h=1./M;
   
   // Solution vector
-  std::vector<double> theta(M+1);
+  std::vector<double> theta(M+1,0);
   
-  // Gauss Siedel is initialised with a linear variation
-  // of T
-  
-  for(unsigned int m=0;m <= M;++m)
-     theta[m]=(1.-m*h)*(To-Te)/Te;
-  
-  // Gauss-Seidel
-  // epsilon=||x^{k+1}-x^{k}||
-  // Stopping criteria epsilon<=toler
-  
-  int iter=0;
-  double xnew,epsilon;
-  if (norm=="L2"){  
+// matrix
+   std::vector <double> c(M-1,-1);
+   std::vector <double> b(M-1,-1);
+   std::vector <double> a(M,1);
+   for(int i=0;i<M-1;i++)
+       a[i]= 2+ h*h*act;
+ // termine noto
+    std::vector <double> g(M,0);
+    g[0]=(To-Te)/Te;
+     
+ // Thomas algorithm 
+   
+ // fattorizzazione
+    for(int i=0;i<M-1;i++){
+	b[i]=b[i]/a[i];
+        a[i+1]=a[i+1]-b[i]*c[i];
+     }
+     
+  // soluzione
+     std::vector<double> y(M);
+     y[0]=g[0];
+     for (int i=1; i<M;i++)
+          y[i]=g[i]-b[i-1]*y[i-1];
 
-     do
-       { epsilon=0.;
-
-	  
-
-         for(int m=1;m < M;m++)
-         {   
-	   xnew  = (theta[m-1]+theta[m+1])/(2.+h*h*act);
-	   epsilon += h*(xnew-theta[m])*(xnew-theta[m]);
-	   theta[m] = xnew;
-         }
-
-	 //Last row
-	 xnew = theta[M-1]; 
-	 epsilon += h/2*(xnew-theta[M])*(xnew-theta[M]);
-	 theta[M]=  xnew; 
-
-	 iter=iter+1;     
-       }while((sqrt(epsilon) > toler) && (iter < itermax) );
-
-    if(iter<itermax)
-      cout << "M="<<M<<"  Convergence in "<<iter<<" iterations"<<endl;
-    else
-      {
-	cerr << "NOT CONVERGING in "<<itermax<<" iterations "<<
-	  "||dx||="<<sqrt(epsilon)<<endl;
-	status=1;
-      }
-    }
-    if (norm=="H1"){
-        double grad,xold;
-        do
-       { epsilon=0.;
- 	
-	 xold = theta[0]; 
-	 
-         for(int m=1;m < M;m++)
-         { 
-	   xnew  = (theta[m-1]+theta[m+1])/(2.+h*h*act);
-           grad= (xnew-theta[m-1])-(theta[m]-xold);
-	   epsilon += h*(xnew-theta[m])*(xnew-theta[m])+grad*grad/h;
-           xold=theta[m];
-	   theta[m] = xnew;
-           
-         }
-
-	 //Last row
-	 xnew = theta[M-1]; 
-         grad= (xnew-theta[M]+xold-theta[M-1]);
- 
-	 epsilon += h*(xnew-theta[M])*(xnew-theta[M])+grad*grad/h;
-	 theta[M]=  xnew; 
-
-	 iter=iter+1; 
-               
-       }while((sqrt(epsilon) > toler) && (iter < itermax) );
-	 if(iter<itermax)
-   	   cout << "M="<<M<<"  Convergence in "<<iter<<" iterations"<<endl;
-    	else
-    	  {
-	cerr << "NOT CONVERGING in "<<itermax<<" iterations "<<
-	  "||dx||="<<sqrt(epsilon)<<endl;
-	status=1;
-      }
-    }
-
+     theta[M]=y[M-1]/a[M-1];
+     for(int i=1;i<M;i++)
+       theta[M-i]=(y[M-i-1]-c[M-i-1]*theta[M-i+1])/a[M-i-1];
+     theta[0]=(To-Te)/Te;
  // Analitic solution
-
-    vector<double> thetaa(M+1);
+    
+    vector<double> thetaa(M+1,0);
      for(int m=0;m <= M;m++)
        thetaa[m]=Te+(To-Te)*cosh(sqrt(act)*(1-m*h))/cosh(sqrt(act));
 
      // writing results with format
      // x_i u_h(x_i) u(x_i) and lauch gnuplot 
 
-     //Gnuplot gp;
+     Gnuplot gp;
      std::vector<double> coor(M+1);
      std::vector<double> sol(M+1);
      std::vector<double> exact(M+1);
@@ -179,9 +125,9 @@ int main(int argc, char** argv)
 	   std::make_tuple(m*h*L,Te*(1.+theta[m]),thetaa[m]);
        }
      // Using temporary files (another nice use of tie)
-     /*gp<<"plot"<<gp.file1d(std::tie(coor,sol))<<
+     gp<<"plot"<<gp.file1d(std::tie(coor,sol))<<
        "w lp title 'uh',"<< gp.file1d(std::tie(coor,exact))<<
-       "w l title 'uex'"<<std::endl;*/
+       "w l title 'uex'"<<std::endl;
      f.close();
      return status;
 }
